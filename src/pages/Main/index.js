f953bd42dd7ad17cdd75fd0a37eeb759733073b2
Hillel1408 <etemax@bk.ru>
2024-01-27 14:25:33 +0300

    верстка кнопок на главной && тултипы && ховеры

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,14 +3,30 @@ import { Switcher } from "../../components";
 import styles from "./Main.module.scss";
 
 export default function Main() {
+    const list = [
+        "государственные корпорации;",
+        "государственные компании;",
+        "публично-правовые компании;",
+        "субъекты естественных монополий;",
+        "организации, осуществляющие регулируемые виды деятельности в сфере электроснабжения, газоснабжения, теплоснабжения, водоснабжения, водоотведения, очистки сточных вод, обращения с твердыми коммунальными отходами, автономными учреждениями, хозяйственные общества, в уставном капитале которых доля участия Российской Федерации, субъекта Российской Федерации, муниципального образования в совокупности превышает 50%;",
+        "дочерние хозяйственные общества, в уставном капитале которых более 50% долей в совокупности принадлежит указанным выше юридическим лицам;",
+        "дочерние хозяйственные общества, в уставном капитале которых более 50% долей в совокупности принадлежит указанным выше дочерним хозяйственным обществам.",
+    ];
+
     return (
         <div className="container">
             <div className={styles.header}>
                 <div className={styles.headerFlex}>
                     <div>
-                        <button className={classNames("icon-deployed-code-alert", "text-xs", styles.headerBtn)}>
-                            Почему моей компании нужно переходить на корпоративные закупочные стандарты Группы Газпром?
-                        </button>
+                        <div className={styles.headerBtn}>
+                            <button className={classNames("icon-deployed-code-alert", "text-xs")}>
+                                Почему моей компании нужно переходить на корпоративные закупочные стандарты Группы Газпром?
+                            </button>
+                            <p className="text-xs">
+                                Компании Группы Газпром ведут свою закупочную деятельность в соответствии с едиными правилами и в соответствии с принципами,
+                                установленными в Положении о закупках товаров, работ, услуг ПАО «Газпром» и Компаний Группы Газпром — Положение ПАО «Газпром».
+                            </p>
+                        </div>
                         <h2 className="subtitle">Что необходимо сделать для организации закупочного деятельности по единым требованиям в Группе Газпром?</h2>
                         <p className="text-s">
                             Для организации закупочного процесса в соответствии с корпоративными правилами Группы Газпром необходимо сформировать инфраструктуру
@@ -23,7 +39,14 @@ export default function Main() {
                                 <p className={classNames("text-medium-xs", "icon-notification-important")}>
                                     Обратите внимание, состав мероприятий относится к заказщикам, работающим по 223-ФЗ.
                                 </p>
-                                <button className={classNames(styles.headerLinkBtn, "text-s", "icon-chevron-right")}>Кто к ним относится</button>
+                                <div className={styles.headerLinkBtn}>
+                                    <button className={classNames("text-s", "icon-chevron-right")}>Кто к ним относится</button>
+                                    <ul className="text-s">
+                                        {list.map((item, index) => (
+                                            <li key={index}>{item}</li>
+                                        ))}
+                                    </ul>
+                                </div>
                             </div>
                             <Switcher />
                         </div>
@@ -32,6 +55,14 @@ export default function Main() {
                         </button>
                     </div>
                 </div>
+            </div>
+            <div className={styles.navigation}>
+                <img src="/images/main/main.webp" alt=""></img>
+                <button className={classNames("icon-box-edit")}>Создать нормативную базу</button>
+                <button className={classNames("icon-display-settings")}>Организовать работу в информационных системах</button>
+                <button className={classNames("icon-groups")}>Наладить взаимодействие участников процесса</button>
+                <button className={classNames("icon-monitoring")}>Настроить отдельные элементы планирования</button>
+                <button className={classNames("icon-folder-open")}>Обеспечить контроль, работу с договорами, формирование отчетности</button>
             </div>
         </div>
     );
